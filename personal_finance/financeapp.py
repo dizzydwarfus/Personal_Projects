5343126e84f1b8d1d05d830a3c1e2b6127573e6c
dizzydwarfus <67429119+dizzydwarfus@users.noreply.github.com>
2022-12-26 22:10:00 +0100

    26-12-2022

--- personal_finance/financeapp.py
+++ personal_finance/financeapp.py
@@ -1,25 +1,85 @@
 import pandas as pd
 import streamlit as st
 import datetime as dt
+import requests
+import json
+from tickerlist import tickers
 
 #####################################################
 
-# Streamlit page title
+# Define dropdowns and conditions
 
 #####################################################
 
-st.write("""
+page_placeholder = st.sidebar.selectbox('What do you want to see?', [
+                                        'Financial Statements', 'Dashboard', 'Charts'])
 
-# Personal Finance Overview
+ticker_list_box = st.sidebar.selectbox("Select a ticker symbol:", tickers)
 
-""")
+company_statements = ['income-statement',
+                      'cash-flow-statement', 'balance-sheet-statement']
 
-labels = ['show dataframe', 'categorize transactions', 'show categories of spending',
-          'show piechart', 'show slider to adjust for date', 'show slider to adjust for type of spending']
-with st.sidebar:
-    for i, value in enumerate(labels):
-        if st.checkbox(labels[i]):
-            st.write("Done!")
+#####################################################
+
+# FInancial Statements Page
+
+#####################################################
+
+if page_placeholder == 'Financial Statements':
+
+    st.write(f"""
+
+    # {ticker_list_box}
+
+    """)
+    for i, x in enumerate(company_statements):
+        if st.checkbox(f'Show {x}'):
+            st.header(f'{x.capitalize()}')
+            with open(f'D:\lianz\Desktop\Python\data_science_discovery\personal_finance\{x}\{ticker_list_box}.json', 'r') as f:
+                statement = json.load(f)
+
+            year_selection = st.selectbox(
+                "Select year:", range(len(statement)), format_func=lambda x: f"{statement[x]['calendarYear']}")
+
+            st.write(statement[year_selection])
+
+
+#####################################################
+
+# Dashboard Page
+
+#####################################################
+
+
+#####################################################
+
+# Charts Page
+
+#####################################################
+
+if page_placeholder == 'Charts':
+    st.title(f"""
+
+    {ticker_list_box}
+
+    """)
+
+    st.write(f"""
+    
+    ## Cash Flow (last 5 years)
+
+    """)
+    with open(f'D:\lianz\Desktop\Python\data_science_discovery\personal_finance\{company_statements[1]}\{ticker_list_box}.json', 'r') as f:
+        statement = json.load(f)
+
+    year_range = st.slider('View years:', min_value=int(
+        statement[-1]['calendarYear']), max_value=int(statement[0]['calendarYear']))
+
+    cash_chart = pd.DataFrame.from_dict(statement[0])
+
+    st.line_chart(cash_chart)
+    st.write(year_range)
+
 
 #####################################################
 
@@ -27,20 +87,21 @@ with st.sidebar:
 
 #####################################################
 
-st.write("""
+# st.write("""
 
-## Transactions Log
+# ## Transactions Log
 
-All transactions downloaded from banking website.
+# All transactions downloaded from banking website.
 
-Categories of each transactions are categorized using *classification* algorithm.
+# Categories of each transactions are categorized using *classification* algorithm.
 
-""")
+# """)
 
-df = pd.read_csv("D:\lianz\Desktop\Python\data_science_discovery\personal_finance\expenses.csv",
-                 sep=',', parse_dates=['Datum'])
-df['Datum'] = df['Datum'].dt.date
+# df = pd.read_csv("D:\lianz\Desktop\Python\data_science_discovery\personal_finance\expenses.csv",
+#                  sep=',', parse_dates=['Datum'])
+# df['Datum'] = df['Datum'].dt.date
 
-# df_checkbox = st.checkbox(label="Show Transactions Log", key='df_checkbox', value=True)
+# df_checkbox = st.checkbox(label="Show Transactions Log",
+#                           key='df_checkbox', value=True)
 # if df_checkbox:
 #     st.dataframe(df)
