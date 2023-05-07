bf24a5e5bdae356830959eac0bf4920472864f02
dizzydwarfus <67429119+dizzydwarfus@users.noreply.github.com>
2023-05-08 00:19:55 +0200

    setup db connection/test

--- nba/sql/sql_connector.py
+++ nba/sql/sql_connector.py
@@ -4,53 +4,53 @@ from sqlalchemy.engine import URL
 import os
 from dotenv import load_dotenv
 from functools import partial
+from dataclasses import dataclass
 
 load_dotenv()
 
 
-def create_connection_url(server, database, username, password, port, driver):
-    connection_url = URL.create(
-        "mssql+pyodbc",
-        username=username,
-        password=password,
-        host=server,
-        port=port,
-        database=database,
-        query={
-            "driver": driver,
-            # "TrustServerCertificate": "yes",
-            # "authentication": "ActiveDirectoryIntegrated",
-        }
-    )
-    return connection_url
+@dataclass
+class Database:
+    database: str
+    server: str = os.environ.get('server')
+    username: str = os.getenv('DB_username')
+    password: str = os.getenv('DB_password')
+    port: str = os.getenv('port')
+    driver: str = 'ODBC Driver 17 for SQL Server'
+
+    def __post_init__(self):
+        self.connection_url = self.create_connection_url()
+        self.engine = self.create_db_engine()
+
+    def create_connection_url(self):
+        self.connection_url = URL.create(
+            "mssql+pyodbc",
+            username=self.username,
+            password=self.password,
+            host=self.server,
+            port=self.port,
+            database=self.database,
+            query={
+                "driver": self.driver,
+                # "TrustServerCertificate": "yes",
+                # "authentication": "ActiveDirectoryIntegrated",
+            }
+        )
+        return self.connection_url
+
+    def create_db_engine(self):
+        self.engine = create_engine(url=self.connection_url, echo=True)
+        return self.engine
+
+    def test_connection(self):
+        try:
+            with self.engine.connect() as conn:
+                result = conn.execute(text("SELECT DB_NAME()")).fetchone()
+            assert result[0] is not None
+            print(f'Connection to {result[0]} was successful.')
+        except Exception as e:
+            # If the connection fails, the test will fail with an exception
+            assert False, f'Connection test failed: {e}'
 
 
-def test_connection(connection_string):
-    try:
-        # Replace the connection string with your own
-        connection_string = connection_string
-        engine = create_engine(connection_string, echo=True)
-        with engine.connect() as conn:
-            result = conn.execute(text("SELECT DB_NAME()")).fetchone()
-        assert result[0] is not None
-        print(f'Connection to {result[0]} was successful.')
-    except Exception as e:
-        # If the connection fails, the test will fail with an exception
-        assert False, f'Connection test failed: {e}'
-
-
-server = os.environ.get('server')
-database = 'NBA'
-username = os.getenv('DB_username')
-password = os.getenv('DB_password')
-port = os.getenv('port')
-driver = 'ODBC Driver 17 for SQL Server'
-
-CreateURL = partial(create_connection_url, server=server,
-                    username=username, password=password, port=port, driver=driver,)
-
-nba_connection = CreateURL(
-    database=database)
-
-
-test_connection(connection_string=nba_connection)
+Database(database='NBA',).test_connection()
--- nba/webapp.py
+++ nba/webapp.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from draw_baskbetball_court import *
+import pandas as pd
+
 
 st.set_page_config(page_title="NBA Exploratory Data",
                    page_icon=":magnifying_glass_tilted_left:",
@@ -13,13 +15,14 @@ st.markdown("""
 
 
 """)
+custom = pd.read_csv(
+    r'D:\lianz\Desktop\Python\personal_projects\nba\data\games_played.csv')
 
 fig = go.Figure()
-draw_plotly_court_v2(fig, fig_width=1000, x_cal=250, y_cal=-417.5)
+draw_plotly_court(fig, fig_width=1000, x_cal=250, y_cal=-417.5)
 fig.add_trace(go.Scatter(x=[250, 100, 400,], y=np.array([417.5, 100, 300]) * -1,
-                         customdata=[[5, 'Lebron James', '1st Quarter', '10:11', '1st quarter, 11:42.0 remaining<br>Austin Reaves missed 2-pointer from 4 ft<br>LA Lakers tied 0-0'], [
-                             30, 'Stephen Curry', '2nd Quarter', '5:42'], [15, 'Anthony Davis', '4th Quarter', '0:12']],
-                         mode='markers', marker=dict(color='red', size=20),
+                         customdata=custom,
+                         mode='markers', marker=dict(color='red', size=20), name='make',
                          hovertemplate="<b>Distance</b>: %{customdata[0]}ft"
                          + "<br><b>Player</b>: %{customdata[1]}"
                          + "<br><b>Quarter</b>: %{customdata[2]}"
@@ -31,5 +34,6 @@ fig.update_layout(hovermode='closest',
                                   font_family='Rockwell'),
                   )
 
-st.plotly_chart(fig, use_container_width=True)
+st.plotly_chart(fig, use_container_width=False)
+
 st.write(fig.data)
