3e9b2e4a77970bfda10d7f98c3e79d1cf9e65d8e
dizzydwarfus <67429119+dizzydwarfus@users.noreply.github.com>
2023-09-25 00:47:00 +0200

    moved nba-api to a new repo

--- nba_api/app/api/endpoints/teams.py
+++
@@ -1,6 +0,0 @@
-from fastapi import APIRouter, Depends, HTTPException
-from ...database.sql_connector import DB
-from ...database.db import get_db
-from sqlalchemy import text
-
-router = APIRouter()
