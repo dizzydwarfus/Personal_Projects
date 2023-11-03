fe6b8d4257a273587060bb906c3a128ead94ff2a
dizzydwarfus <67429119+dizzydwarfus@users.noreply.github.com>
2023-11-03 23:25:34 +0100

    scraped AH data into excel with nutritional info

--- grocery_list/models/models.py
+++ grocery_list/models/models.py
@@ -1,67 +1,67 @@
 from pydantic import BaseModel
 from datetime import datetime
-from typing import Union, List
+from typing import Union, List, Optional
 
 
 class ProductCard(BaseModel):  # use search_products(query='') method
     webshopId: int  # this is the productId
     hqId: int
-    title: str
-    salesUnitSize: str
-    unitPriceDescription: str
-    images: list
-    priceBeforeBonus: float
+    title: Optional[str] = None
+    salesUnitSize: Optional[str] = None
+    unitPriceDescription: Optional[str] = None
+    images: Optional[list] = None
+    priceBeforeBonus: Optional[float] = None
     orderAvailabilityStatus: str
-    mainCategory: str
-    subCategory: str
+    mainCategory: Optional[str] = None
+    subCategory: Optional[str] = None
     subCategoryId: int
-    brand: str
-    shopType: str
-    availableOnline: bool
-    isPreviouslyBought: bool
-    descriptionHighlights: str
-    propertyIcons: list
-    properties: dict
-    nix18: bool
-    isStapelBonus: bool
-    extraDescriptions: list
-    isBonus: bool
-    descriptionFull: str
-    isOrderable: bool
-    isInfiniteBonus: bool
-    isSample: bool
-    isVirtualBundle: bool
-    isFavorite: bool
-    minBestBeforeDays: int
-    discountLabels: list
+    brand: Optional[str] = None
+    shopType: Optional[str] = None
+    availableOnline: Optional[bool] = None
+    isPreviouslyBought: Optional[bool] = None
+    descriptionHighlights: Optional[str] = None
+    propertyIcons: Optional[list] = None
+    properties: Optional[dict] = None
+    nix18: Optional[bool] = None
+    isStapelBonus: Optional[bool] = None
+    extraDescriptions: Optional[list] = None
+    isBonus: Optional[bool] = None
+    descriptionFull: Optional[str] = None
+    isOrderable: Optional[bool] = None
+    isInfiniteBonus: Optional[bool] = None
+    isSample: Optional[bool] = None
+    isVirtualBundle: Optional[bool] = None
+    isFavorite: Optional[bool] = None
+    minBestBeforeDays: Optional[int] = None
+    discountLabels: Optional[list] = None
 
 
 class ProductDetails(BaseModel):
     webshopId: int  # this is the productId
     hqId: int
-    title: str
-    salesUnitSize: str
-    unitPriceDescription: str
-    priceBeforeBonus: float
-    orderAvailabilityStatus: str
-    mainCategory: str
-    subCategory: str
+    title: Optional[str] = None
+    salesUnitSize: Optional[str] = None
+    unitPriceDescription: Optional[str] = None
+    priceBeforeBonus: Optional[float] = None
+    orderAvailabilityStatus: Optional[str] = None
+    mainCategory: Optional[str] = None
+    subCategory: Optional[str] = None
     subCategoryId: int
-    brand: str
-    shopType: str
-    availableOnline: bool
-    isPreviouslyBought: bool
-    descriptionHighlights: str
-    nix18: bool
-    isStapelBonus: bool
-    isBonus: bool
-    descriptionFull: str
-    isOrderable: bool
-    isInfiniteBonus: bool
-    isSample: bool
-    isVirtualBundle: bool
-    isFavorite: bool
-    minBestBeforeDays: int
+    brand: Optional[str] = None
+    shopType: Optional[str] = None
+    availableOnline: Optional[bool] = None
+    isPreviouslyBought: Optional[bool] = None
+    descriptionHighlights: Optional[str] = None
+    nix18: Optional[bool] = None
+    isStapelBonus: Optional[bool] = None
+    isBonus: Optional[bool] = None
+    descriptionFull: Optional[str] = None
+    isOrderable: Optional[bool] = None
+    isInfiniteBonus: Optional[bool] = None
+    isSample: Optional[bool] = None
+    isVirtualBundle: Optional[bool] = None
+    isFavorite: Optional[bool] = None
+    minBestBeforeDays: Optional[int] = None
 
 
 class ProductPrice(BaseModel):
@@ -74,14 +74,7 @@ class ProductPrice(BaseModel):
 
 class NutritionalInfo(BaseModel):  # use get_product_details(productId) method
     productId: int
-    energyKcal: float  # ENER- / energie
-    energyKj: float  # ENER- / energie
-    fat: float  # FAT / vet
-    saturatedFat: float  # FASAT / waarvan verzadigd
-    unsaturatedFat: float  # X_FUNS / waarvan onverzadigd
-    carbohydrates: float  # CHOAVL / koolhydraten
-    sugars: float  # SUGAR- / waarvan suikers
-    dietaryFiber: float  # FIBTG / Voedingsvezel
-    protein: float  # PROT / eiwitten
-    salt: float  # SALTEQ / zout
-    sodium: float
+    nutrientType: str
+    nutrientValue: float
+    unit: str
+    unitLabel: str
